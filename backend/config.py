71a475650aa7ae87cc6325f51948097e69f8d687
Klaus Wong <>
2023-06-04 23:48:51 +0800

    dockerize frontend

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 import os
 
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "postgresql://user:password@localhost:5432/database")
-BUCKET_NAME = os.getenv("BUCKET_NAME", "demo-bucket-dododo2")
-PUBSUB_TOPIC = os.getenv("PUBSUB_TOPIC", "projects/bold-network-380012/topics/ocr-project")
+BUCKET_NAME = os.getenv("BUCKET_NAME", "demo-dododo3")
+PUBSUB_TOPIC = os.getenv("PUBSUB_TOPIC", "projects/thematic-garage-386114/topics/dg-library")
 REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
 REDIS_PORT = os.getenv('REDIS_PORT', 6379)
