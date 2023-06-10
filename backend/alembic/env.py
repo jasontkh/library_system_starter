fcd15bc566b4f8422c8957610b553bf9f0bcf8e2
Klaus Wong <>
2023-06-11 01:19:31 +0800

    use SQLALCHEMY_DATABASE_URL for all db conn string

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -9,7 +9,7 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", os.getenv("SQLALCHEMY_URL", "postgresql://user:password@postgres:5432/database"))
+config.set_main_option("sqlalchemy.url", os.getenv("SQLALCHEMY_DATABASE_URL", "postgresql://user:password@postgres:5432/database"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
